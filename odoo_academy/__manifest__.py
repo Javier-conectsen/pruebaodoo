7179045cbcc2f7dd629383d7122c6604f0eab2ef
javier-conectsen <javier.gonzalez@conectsen.com>
2021-10-04 21:11:36 +0000

    first module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,32 @@
+# _*_ coding: utf-8 _*_
+
+{
+    'name': 'Odoo Academy',
+    
+    'sumary': """Academy app to manage Training""",
+    
+    'description': """
+        Academy Module to manage Training:
+        - Courses
+        - Sessions
+        - Attendees
+    """,
+    
+    'autor': 'Odoo',
+    
+    'website':'http://www.odoo.com',
+    
+    'category': 'Training',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo':[
+       
+    ],
+}
